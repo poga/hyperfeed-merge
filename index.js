e6a4cb47d79f8c51600bb16183ddb22d1cb3b054
Poga Po <poga.bahamut@gmail.com>
2016-10-06 13:44:08 +0800

    add source feed key

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function merge (ins, out) {
 
   out.setMeta({title: 'merge', description: ins.map(f => f.key().toString('hex')).join(',')})
   ms.on('data', x => {
-    out.push(x.item)
+    out.push(Object.assign({}, x.item, {"hf:merge:source": x.source}))
   })
 
   return out
@@ -20,7 +20,7 @@ function merge (ins, out) {
 function transform (feed) {
   return function (entry, enc, cb) {
     feed.load(entry).then((item) => {
-      this.push({entry: entry, item: item, owner: feed.key().toString('hex')})
+      this.push({entry: entry, item: item, source: feed.key().toString('hex')})
       cb()
     })
   }
